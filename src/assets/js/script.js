952d4df1ec755409b426a224cadf4bea031c87c0
harshkumar9896 <its.harshkumar2006@gmail.com>
2025-12-05 22:08:37 +0530

    added alert

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,5 +1,11 @@
-export function login(){
-    const username=document.getElementById("Username").value;
-    const password=document.getElementById("password").value;
-    
+export function login() {
+    const username = document.getElementById("Username").value;
+    const password = document.getElementById("password").value;
+
+    if (username.length < 6 && password.length < 10) {
+        alert("username or password is not valid")
+
+    } else {
+        alert("Your Username is " + username + "\nYour Password is " + password)
+    }
 }
